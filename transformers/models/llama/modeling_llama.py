8b49045004fa34c4bfd12922fa3c792dac5479d1
Zhenrui Yue <zyue@Zhenruis-MBP.local>
2025-05-26 23:42:01 -0700

    llama

--- transformers/models/llama/modeling_llama.py
+++ transformers/models/llama/modeling_llama.py
@@ -488,6 +488,29 @@ LLAMA_INPUTS_DOCSTRING = r"""
 """
 
 
+class ThinkingResidualLambda(nn.Module):
+    c = 8.0
+
+    def __init__(self, config: LlamaConfig):
+        super().__init__()
+        self.Lambda = nn.Parameter(torch.randn(config.hidden_size))
+
+    def reset_lambda_parameters(
+            self, r_min=0.9, r_max=0.999
+        ):
+        with torch.no_grad():
+            nn.init.uniform_(self.Lambda, a=r_min, b=r_max)
+            self.Lambda.data.copy_(
+                - torch.log((self.Lambda ** (-1. / self.c) ) - 1)
+            )
+
+    def forward(self, r_t):
+        a_t = torch.exp(
+            - self.c * nn.functional.softplus(-self.Lambda, beta=1, threshold=20) * r_t
+        )
+        return a_t
+
+
 @add_start_docstrings(
     "The bare LLaMA Model outputting raw hidden-states without any specific head on top.",
     LLAMA_START_DOCSTRING,
@@ -513,6 +536,10 @@ class LlamaModel(LlamaPreTrainedModel):
         self.rotary_emb = LlamaRotaryEmbedding(config=config)
         self.gradient_checkpointing = False
 
+        self.thinking_residual_gate_r = nn.Linear(config.hidden_size, config.hidden_size)
+        self.thinking_residual_gate_i = nn.Linear(config.hidden_size, config.hidden_size)
+        self.thinking_residual_Lambda = ThinkingResidualLambda(config)
+
         # Initialize weights and apply final processing
         self.post_init()
 
@@ -521,6 +548,12 @@ class LlamaModel(LlamaPreTrainedModel):
 
     def set_input_embeddings(self, value):
         self.embed_tokens = value
+
+    def thinking_residual(self, embeds, residual, eps=1e-8):
+        r_t = torch.sigmoid(self.thinking_residual_gate_r(embeds))
+        i_t = torch.sigmoid(self.thinking_residual_gate_i(embeds))
+        a_t = self.thinking_residual_Lambda(r_t)
+        return a_t * embeds + torch.sqrt(1 - a_t.pow(2) + eps) * (i_t * residual), a_t
 
     @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
     def forward(
