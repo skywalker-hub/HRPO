4a903286690977f1875102016b3d2eef72218e1f
skywalker-hub <1745826062@qq.com>
2026-02-17 18:51:48 +0800

    调试

--- eval_gsm8k_test.py
+++ eval_gsm8k_test.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     # 直接写死为你启动命令中的配置
     greedy = False
     batch_size = 2
-    checkpoint_path = "/root/autodl-tmp/HRPO/experiments/Qwen2.5-1.5B-Instruct-gsm8k-group4-lora32-rmin0.99-temp0.5/checkpoint-934"
+    checkpoint_path = "/root/autodl-tmp/HRPO/test0116.2.0.4/Qwen2.5-1.5B-Instruct-gsm8k-group4-lora32-lr0.01-init-2-rmin0.981-temp0.5/checkpoint-934"
 
     base_model = None
     ### 本地模型路径映射
