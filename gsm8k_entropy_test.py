8fbda1c17bd8ec59e867ad4405eb22fae288615b
skywalker-hub <1745826062@qq.com>
2026-02-26 22:18:28 +0800

    熵分析

--- gsm8k_entropy_test.py
+++ gsm8k_entropy_test.py
@@ -14,7 +14,7 @@ from utils import *
 
 
 # ====== 在此处修改测试问题 ======
-QUESTION = "Kylar went to the store to buy glasses for his new apartment. One glass costs $5, but every second glass costs only 60% of the price. Kylar wants to buy 16 glasses. How much does he need to pay for them?"
+QUESTION = "A new program had 60 downloads in the first month. The number of downloads in the second month was three times as many as the downloads in the first month, but then reduced by 30% in the third month. How many downloads did the program have total over the three months?"
 # ================================
 
 
