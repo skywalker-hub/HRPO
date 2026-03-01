ecf2a995b9e2ebcd2c1ace7106aad8295cb7225a
skywalker-hub <1745826062@qq.com>
2026-03-01 16:18:54 +0800

    sota分析

--- gsm8k_entropy_test.py
+++ gsm8k_entropy_test.py
@@ -658,7 +658,7 @@ def run_entropy_test(
 
 if __name__ == "__main__":
     # ====== 在此处手动修改参数，直接运行即可调试 ======
-    checkpoint_path = "/root/autodl-tmp/HRPO/test223.1/Qwen2.5-1.5B-Instruct-gsm8k-group4-lora32-lr0.01-init-2-rmin0.981-temp0.5/checkpoint-934"  # 修改为你的 adapter 路径
+    checkpoint_path = "/root/autodl-tmp/HRPO/test301.1/Qwen2.5-3B-Instruct-gsm8k-group4-lora32-lr0.01-init-2-rmin0.981-temp0.5/checkpoint-934"  # 修改为你的 adapter 路径
     temperature = 0.5
     is_inference = False   # True = greedy, False = sampling
     # ================================================
